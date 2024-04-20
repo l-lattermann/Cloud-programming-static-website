32665352fccd2bec4cacd651c5aa377ac91ad8ba
Laurenz <laurenz.lattermann@gmail.com>
2024-04-20 09:23:30 +0200

    changed credential path

--- config.tf
+++ config.tf
@@ -11,7 +11,7 @@ terraform {
 # Configure the AWS Provider
 provider "aws" {
   profile =                 var.profile
-  shared_credentials_files  = ["${var.credential_path}"]
+  shared_credentials_files  = [var.credential_path]
   region =                  var.aws_region
 }
 
