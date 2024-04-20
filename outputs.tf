689f698443abc0a14ef863ce2f459488b1b700c3
Laurenz <laurenz.lattermann@gmail.com>
2024-04-21 00:06:14 +0200

    EOT output website link

--- outputs.tf
+++ outputs.tf
@@ -1,5 +1,14 @@
 ## Output the complete link to the index.html file at hte end of the terraform build
 output "Website_index_URL" {
-  value = "https://${aws_cloudfront_distribution.static_website.domain_name}/index.html"
+  value = <<-EOT
+
+
+
+  Link to the Website:
+  https://${aws_cloudfront_distribution.static_website.domain_name}/index.html
+
+
+
+EOT
   description = "This is the link to the index file of the website. Copy and paste it in your browser"
 }
